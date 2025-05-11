de921b7f81c18892d8f3e01c73e8832fc7a78688
nitish624 <nk3394841245@gmail.com>
2025-05-11 19:39:14 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+
+function exploreCourses() {
+  alert("Course details will be available soon.");
+}
+
+function submitForm(event) {
+  event.preventDefault();
+  alert("Your doubt has been submitted!");
+}
